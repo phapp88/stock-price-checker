f9e25c2552fa895d95ed6f394b32638e7a85ec91
Paul <phapp1988@gmail.com>
2018-09-10 13:04:50 -0500

    Implement project user stories

---
+++ tests/1_unit-tests.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+//var StockHandler = require('../controllers/stockHandler.js');
+
+//var stockPrices = new StockHandler();
+
+suite('Unit Tests', function(){
+
+//none required
+
+});
